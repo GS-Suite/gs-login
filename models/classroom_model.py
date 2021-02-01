93a8b9525157b960d60353d382eef4821a5e53ca
sansqra <sqrasan@gmail.com>
2021-02-01 20:01:01 +0530

    unique class id for invite

--- models/classroom_model.py
+++ models/classroom_model.py
@@ -6,6 +6,9 @@ from sqlalchemy.sql.sqltypes import DateTime
 from fastapi_sqlalchemy import db
 from models.base import Base
 import datetime
+import string
+import random
+import re
 
 
 class Classroom(Base):
@@ -16,13 +19,27 @@ class Classroom(Base):
     class_name = Column(String)
     # enrolled_users = Column(JSON)
     created_time = Column(DateTime, default=datetime.datetime.now())
+    unique_id = Column(Integer)
+
+
+def id_generator(class_name, created_time):
+    size = 9
+    charSet = string.ascii_lowercase + string.ascii_uppercase + \
+        string.digits + class_name + created_time
+    charSet = re.sub(r"\s+", "", charSet)
+    charSet = charSet.strip()
+    return ''.join(random.choice(charSet) for _ in range(size))
 
 
 async def create_classroom(user_id, classroom):
     new_class = Classroom(
         creator_id=user_id,
         class_name=classroom.class_name,
-        created_time=datetime.datetime.now()
+        created_time=datetime.datetime.now(),
+        unique_id=id_generator(
+            class_name=classroom.class_name,
+            created_time=str(datetime.datetime.now())
+        )
     )
     try:
         db.session.add(new_class)
